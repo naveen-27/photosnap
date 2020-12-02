f4fb2a2fa2025c89477a52f203e108ba26fca2fb
Naveen Kumar <456endson@gmail.com>
2020-12-02 16:02:37 +0530

    Made the Button component accept tabIndex prop

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -4,7 +4,14 @@ import classes from "../Stylesheets/Button.module.css";
 const Button = (props) => {
   const theme = props.isLight ? classes.light : classes.dark;
 
-  return <button className={`${classes.Btn} ${theme}`}>{props.label}</button>;
+  return (
+    <button
+      className={`${classes.Btn} ${theme}`}
+      tabIndex={props.tabIndex || 0}
+    >
+      {props.label}
+    </button>
+  );
 };
 
 export default Button;
